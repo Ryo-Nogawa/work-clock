00e85a0131accdd7c591fbf76381a1c9e412a65d
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2026-02-25 17:25:07 +0900

    feat: Usersエンティティクラスを追加

---
+++ src/main/java/io/github/ryonogawa/workclock/entity/Users.java
@@ -0,0 +1,41 @@
+package io.github.ryonogawa.workclock.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+import lombok.Getter;
+
+@Table("users")
+@Getter
+public class Users {
+
+    @Id
+    @Column("id")
+    private long id;
+
+    @Column("email")
+    private String email;
+
+    @Column("password_hash")
+    private String passwordHash;
+
+    @Column("name")
+    private String name;
+
+    @Column("updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column("created_at")
+    private LocalDateTime createdAt;
+
+    public Users(long id, String email, String passwordHash, String name) {
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.name = name;
+        this.updatedAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now();
+    }
+}
