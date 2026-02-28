d44b38f52b040399bd4f2cdd7e67e76530555b18
Ryo Nogawa <69405100+Ryo-Nogawa@users.noreply.github.com>
2026-02-28 13:55:52 +0900

    feat: 出勤打刻コントローラを追加

---
+++ src/main/java/io/github/ryonogawa/workclock/controller/AttendanceController.java
@@ -0,0 +1,28 @@
+package io.github.ryonogawa.workclock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import io.github.ryonogawa.workclock.service.AttendanceService;
+
+@Controller
+@RequestMapping("/attendance")
+public class AttendanceController {
+
+    private final AttendanceService service;
+
+    public AttendanceController(AttendanceService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/clock-in")
+    public String clockInRegistration(@RequestBody long userId, Model model) {
+        service.clockIn(userId);
+        model.addAttribute("attendance_kind", "出勤");
+        return "complete";
+    }
+
+}
